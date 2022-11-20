2dc6c996e03e270d41974ac756455c1f355c2c04
ainohai <>
2022-11-20 17:08:21 +0200

    Initial commit for joulu-POC

---
+++ src/components/canvas.tsx
@@ -0,0 +1,39 @@
+import * as THREE from 'three'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import * as React from 'react'
+import { random } from 'maath'
+import { Points, PointMaterial } from '@react-three/drei'
+import Stats from 'three/examples/jsm/libs/stats.module'
+
+function Stars(props) {
+  const ref: React.MutableRefObject<THREE.Mesh> = useRef()
+  const [sphere] = useState(() => random.inSphere(new Float32Array(3000), { radius: 1 }))
+  useFrame((state, delta) => {
+      ref.current.rotation.x -= -1 * delta / 50
+      ref.current.rotation.y -=  delta / 150
+  })
+  
+  return (
+    <group rotation={[0, 0, Math.PI / 40]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+        <PointMaterial transparent color="#aaa0f0" size={0.005} sizeAttenuation={true} depthWrite={false} />
+      </Points>
+    </group>
+  )
+}
+
+  export default function WinterCanvas() {
+
+  return (
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <fog attach="fog" args={['blue', 0.6, 3]} />
+      <Stars/>
+    </Canvas>
+  )
+}
+
+
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: "development",
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+        patterns: [
+        { from: 'node_modules/@picocss/pico/css/pico.min.css', to: 'css/'}
+      ]}),
+    new HtmlWebpackPlugin({
+        cache: false,
+      template: 'src/index.html'
+    }),
+    new HtmlWebpackTagsPlugin({
+        tags: [
+          'css/pico.min.css',
+          { path: 'css/pico.min.css' }
+        ],
+        append: false
+      })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+
+};
