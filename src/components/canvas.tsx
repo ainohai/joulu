41ec0a8d86b1bfc5b6a107625da4093303f13875
ainohai <>
2022-11-24 17:27:00 +0200

    Fixed prettifier

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,30 +1,43 @@
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import * as React from 'react'
-import { random } from 'maath'
-import { Points, PointMaterial } from '@react-three/drei'
-import Stats from 'three/examples/jsm/libs/stats.module'
+import * as THREE from 'three';
+import { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import * as React from 'react';
+import { random } from 'maath';
+import { Points, PointMaterial } from '@react-three/drei';
+import Stats from 'three/examples/jsm/libs/stats.module';
 
 function Stars(props) {
-  const ref: React.MutableRefObject<THREE.Mesh> = useRef()
-  const [sphere] = useState(() => random.inSphere(new Float32Array(3000), { radius: 1 }))
+  const ref: React.MutableRefObject<THREE.Mesh> = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(3000), { radius: 1 })
+  );
   useFrame((state, delta) => {
-    ref.current.rotation.x -= -1 * delta / 50
-    ref.current.rotation.y -= delta / 150
-  })
+    ref.current.rotation.x -= (-1 * delta) / 50;
+    ref.current.rotation.y -= delta / 150;
+  });
 
   return (
     <group rotation={[0, 0, Math.PI / 40]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
-        <PointMaterial transparent color="#aaa0f0" size={0.005} sizeAttenuation={true} depthWrite={false} />
+      <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled={false}
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#aaa0f0"
+          size={0.005}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
       </Points>
     </group>
-  )
+  );
 }
 
 export default function WinterCanvas() {
-
   return (
     <Canvas camera={{ position: [0, 0, 1] }}>
       <ambientLight intensity={0.5} />
@@ -33,7 +46,5 @@ export default function WinterCanvas() {
       <fog attach="fog" args={['blue', 0.6, 3]} />
       <Stars />
     </Canvas>
-  )
+  );
 }
-
-
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,52 +1,53 @@
-import * as React from "react"
-import { useCardStore } from "../store/cardStore";
-import { SingleCard } from "../types/card";
+import * as React from 'react';
+import { useCardStore } from '../store/cardStore';
+import { SingleCard } from '../types/card';
 
 type CardProps = {
-    card: SingleCard,
-    index: number,
-    totalNumOfCards: number
+  card: SingleCard;
+  index: number;
+  totalNumOfCards: number;
 };
 
 const Card = (props: CardProps) => {
+  const { card, index, totalNumOfCards } = props;
+  const { visibleCardIndex, nextCard } = useCardStore();
 
-    const { card, index, totalNumOfCards } = props;
-    const { visibleCardIndex, nextCard } = useCardStore()
-
-    return (
-        <article
-            style={{
-                display: index === visibleCardIndex ? "flex" : "none",
-                cursor: index < totalNumOfCards - 1 ? "pointer" : "default",
-            }}
-            className="cardContent"
-            onClick={() => nextCard()}
+  return (
+    <article
+      style={{
+        display: index === visibleCardIndex ? 'flex' : 'none',
+        cursor: index < totalNumOfCards - 1 ? 'pointer' : 'default',
+      }}
+      className="cardContent"
+      onClick={() => nextCard()}
+    >
+      {card.map((cardPart, index) => (
+        <p
+          key={index}
+          style={cardPart.size === 'big' ? { fontSize: '52px' } : {}}
         >
-            {card.map((cardPart, index) =>
-                <p
-                    key={index}
-                    style={cardPart.size === "big" ? { fontSize: "52px" } : {}}>
-                    {cardPart.text}
-                </p>
-            )}
-        </article>
-    )
-
-}
+          {cardPart.text}
+        </p>
+      ))}
+    </article>
+  );
+};
 
 export default function Cards() {
+  const cards = useCardStore((state) => state.cardsOfTheDay);
 
-    const cards = useCardStore((state) => state.cardsOfTheDay);
-
-    return (
-        <main className="container">
-            <div className="centeredCard">
-                {cards.map((card, index) =>
-                    <Card card={card} index={index} totalNumOfCards={cards.length} key={index} />
-                )
-                }
-            </div>
-        </main>
-    )
-
+  return (
+    <main className="container">
+      <div className="centeredCard">
+        {cards.map((card, index) => (
+          <Card
+            card={card}
+            index={index}
+            totalNumOfCards={cards.length}
+            key={index}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
