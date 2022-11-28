1b30b78613e8318ad5208d9785218703c0ccd721
ainohai <>
2022-11-28 08:23:36 +0200

    Added demo backgrounds

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import WinterCanvas from './components/canvas';
 import Cards from './components/cards';
+import { BackgroundImage } from './components/backgroundImage';
 import TestNavigation from './components/testNavigation';
 import './styles.scss';
 
 export default function App() {
   return (
     <>
+      <BackgroundImage />
       <WinterCanvas />
       <Cards />
-      <TestNavigation />
+      {/**<TestNavigation />**/}
     </>
   );
 }
