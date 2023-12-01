b0b53f1c03d24c5a5f3fb48c98ed31b6d93f7fea
ainohai <>
2023-12-01 11:27:19 +0200

    Small timing fix for the image

--- src/components/backgroundImage.tsx
+++ src/components/backgroundImage.tsx
@@ -3,13 +3,16 @@ import joululanssi from '../../assets/joululanssi4.jpg';
 import { useCardStore } from '../store/cardStore';
 
 export function BackgroundImage() {
-  const { setLoading } = useCardStore();
+  const { loading, setLoading } = useCardStore();
   return (
     <div className="joululanssi">
-      <img
-        src={joululanssi}
-        onLoad={() => setTimeout(() => setLoading(false), 500)}
-      />
+      {
+        <img
+          style={{ display: loading ? 'none' : 'inherit' }}
+          src={joululanssi}
+          onLoad={() => setTimeout(() => setLoading(false), 50)}
+        />
+      }
     </div>
   );
 }
